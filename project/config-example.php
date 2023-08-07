c5548bfcbcff4e7bd61efc271273c08ad17aa813
Komron Mukhamadulloev <kmukhamadulloev@gmail.com>
2023-08-07 07:59:40 +0500

    fixed config example file

--- project/config-example.php
+++ project/config-example.php
@@ -5,7 +5,12 @@ define("DB_NAME", "");
 define("DB_USER", "");
 define("DB_PASS", "");
 
-define("SMS_API_KEY", "");
+define("SMS_USER_ID", "your_account_sid");
+define("SMS_AUTH_KEY", "your_api_key");
+define("SMS_NUMBER", "your_twilio_phone_number");
 
-define("MAIL_USERNAME", "");
-define("MAIL_PASSWORD", "");
+define("MAIL_USERNAME", "your_smtp_username");
+define("MAIL_PASSWORD", "your_smtp_password");
+define("MAIL_HOST", "smtp.example.com");
+define("MAIL_PORT", 587);
+define("MAIL_SEC", "tsl");
